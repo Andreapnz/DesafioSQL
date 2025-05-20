726235c4b416262d82031cf53427043f85cf2c52
Andrea Correia <andrea_pnz@hotmail.com>
2025-05-19 22:02:50 -0300

    Add files via upload

--- banco.sql
+++ banco.sql
@@ -41,7 +41,10 @@ fkVaga INT(11),
 data_hora_entrada DATETIME NOT NULL,
 data_hora_saida DATETIME NOT NULL,
 FOREIGN KEY (fkVeiculo) REFERENCES veiculo(idVeiculo),
-FOREIGN KEY (fkVaga) REFERENCES vaga(idVaga));
+FOREIGN KEY (fkVaga) REFERENCES vaga(idVaga);
+
+ALTER TABLE veiculo_vagas
+MODIFY COLUMN data_hora_saida DATETIME;
 
 -- Altere a tabela veiculo para modificar a coluna placa de INT para VARCHAR(8), pois as placas brasileiras -
 -- seguem o padrão Mercosul que tem 8 caracteres (exemplo: ABC1D23). 
@@ -53,7 +56,7 @@ MODIFY COLUMN placa VARCHAR(8);
 ALTER TABLE cliente
 ADD COLUMN email VARCHAR(150);
 
-SELECT * FROM veiculo_vagas;
+SELECT * FROM veiculo;
 
  -- Insira um novo cliente:
 -- Nome: Eduarda Ferreira
@@ -63,12 +66,72 @@ SELECT * FROM veiculo_vagas;
 
 INSERT INTO cliente (nomeCliente, email)
 VALUES ('Eduarda Ferreira', 'eduarda@email.com');
+INSERT INTO cliente (nomeCliente, email) VALUES ('Eduarda Ferreira', 'eduardaFerreira@empresa');
+-- Buscar o id do cliente inserido (exemplo):
+SELECT idCliente FROM cliente WHERE nomeCliente = 'Eduarda Ferreira' AND email = 'eduardaFerreira@empresa';
 
 INSERT INTO veiculo (placa, tipo)
 VALUES ('EFX9D55', 'MOTO');
 
--- Cadastre a entrada do veículo de placa EFX9D55 no estacionamento hoje às 18h na vaga 5
+INSERT INTO cliente_veiculo (fkCliente, fkVeiculo) VALUES (41, 59);
 
-INSERT INTO veiculo_vagas (fkVaga, data_hora_entrada)
-VALUES (SELECT idVaga FROM placa WHERE 
+
+-- Questão 2 — Cadastrar entrada do veículo
+-- Buscar idVeiculo pela placa
+SELECT idVeiculo FROM veiculo WHERE placa = 'EFX9D55';
+-- Suponha idVeiculo = 59
+INSERT INTO veiculo_vagas (fkVeiculo, fkVaga, data_hora_entrada) 
+VALUES (59, 5, '2025-05-19 18:00:00');
+
+-- Questão 3 — Atualizar saída do veículo
+-- Buscar idVeiculo pela placa
+SELECT idVeiculo FROM veiculo WHERE placa = 'EFX9D55';
+-- Suponha idVeiculo = 33
+UPDATE veiculo_vagas
+SET data_hora_saida = '2025-05-19 20:20:00'
+WHERE fkVeiculo = 59 AND data_hora_saida IS NULL;
+
+-- Questão 4 — Atualizar email para três clientes
+select idCliente
+from cliente
+WHERE nomeCliente = 'Bruna Freitas'
+   or nomeCliente = 'Igor Aragão'
+   OR nomeCliente = 'Pietra Lima';
+   
+   
+UPDATE cliente
+SET email = 'empresa@empresa.com'
+WHERE idCliente = 6
+   OR idCliente = 13
+   OR idCliente = 20;
+   
+-- Quantos veículos estão cadastrados no sistema?
+SELECT COUNT(*) AS total_veiculos
+FROM veiculo;
+
+-- Quantos veículos já utilizaram cada tipo de vaga?
+-- Consulta  veiculo_vaga
+SELECT v.tipoVaga, COUNT(DISTINCT vv.fkVeiculo) as total_veiculos
+FROM veiculo_vagas vv
+JOIN vaga v ON vv.fkVaga = v.idVaga
+GROUP BY v.tipoVaga;
+
+-- Mostre em ordem alfabética os nomes e emails dos clientes que possuem motos.
+SELECT c.nomeCliente, c.email
+FROM cliente c
+JOIN cliente_veiculo cv ON c.idCliente = cv.fkCliente
+JOIN veiculo v ON fkVeiculo = v.idVeiculo
+WHERE v.tipo = 'moto'
+ORDER BY c.nomeCliente;
+
+-- Considerando um valor fixo por tipo de veículo (moto: R$8, carro: R$15), calcule o valor total faturado com todos os estacionamentos.
+
+-- Questão 5 (10 pts) — Quantos veículos de cada tipo estacionaram na vaga 8?
+SELECT v.tipo, COUNT(*) AS total_veiculos
+FROM veiculo_vagas vv
+JOIN vaga va ON vv.fkVaga = va.idVaga
+JOIN veiculo V on 
+WHERE va.numVaga = 8
+ORDER BY v.tipoVaga;
+
 
